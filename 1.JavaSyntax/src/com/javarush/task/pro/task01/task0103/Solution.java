3282eb60b076c713d7adafabfe5337adf492f5dd
Володя Никоноров <dantes389@list.ru>
2023-03-18 00:29:12 +0300

    add Solution java syntax task 0105 comment variables

--- 1.JavaSyntax/src/com/javarush/task/pro/task01/task0103/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task01/task0103/Solution.java
@@ -11,15 +11,15 @@ public class Solution {
         String secret = "сверхсекретный";
         String cargo = "груз";
 
-        System.out.print(diego);
+//        System.out.print(diego);
         System.out.print(drop);
-        System.out.print("Амиго");
+//        System.out.print("Амиго");
         System.out.print(" Диего");
         System.out.println();
         System.out.print(drop);
-        System.out.print(secret);
+//        System.out.print(secret);
         System.out.print(" сверхсекретный ");
-        System.out.print("код");
+//        System.out.print("код");
         System.out.print(cargo);
     }
 }
